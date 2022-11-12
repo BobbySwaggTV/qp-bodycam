d45cbd2c58c86067260813b369cd3c9ea8ee4197
BobbySwagg <49619297+BobbySwaggTV@users.noreply.github.com>
2022-11-11 19:13:22 -0600

    ???

--- server.lua
+++ server.lua
@@ -13,10 +13,10 @@ end)
 --RLCore.Functions.CreateUseableItem("bodycam" , function(source, item)
 QBCore.Functions.CreateUseableItem = function(bodycam, cb)
     QBCore.UseableItems[bodycam] = cb
-end    
+end)    
     local _src = source
     TriggerClientEvent("mbl-body:openBoy", _src, item, tonumber(os.date("%H")), tonumber(os.date("%M")), tonumber(os.date("%S")))
-end
+end)
 
 CreateThread(function ()
     while acik do
